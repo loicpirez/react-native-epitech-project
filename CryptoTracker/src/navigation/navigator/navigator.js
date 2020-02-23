58ec57842d235c7a5e3aff0899b57c36463211bc
Etsuro <adrien.bachelet@epitech.Eu>
2020-02-23 18:18:40 +0100

    register/ login

--- CryptoTracker/src/navigation/navigator/navigator.js
+++ CryptoTracker/src/navigation/navigator/navigator.js
@@ -1,5 +1,5 @@
 // navigation.js
-import {Navigation} from 'react-native-navigation';
+import { Navigation } from 'react-native-navigation';
 
 export const goAuth = () => {
   return Navigation.setRoot({
@@ -17,15 +17,15 @@ export const goAuth = () => {
                   visible: true,
                   title: {
                     text: 'Connexion',
-                    alignment: 'center',
-                  },
-                },
-              },
-            },
-          },
-        ],
-      },
-    },
+                    alignment: 'center'
+                  }
+                }
+              }
+            }
+          }
+        ]
+      }
+    }
   });
 };
 
@@ -38,11 +38,113 @@ export const goHome = () => {
           {
             component: {
               id: 'Home',
-              name: 'Home',
-            },
-          },
-        ],
-      },
-    },
+              name: 'Home'
+            }
+          }
+        ]
+      }
+    }
   });
 };
+
+/* Navigation.setRoot({
+  root: {
+    bottomTabs: {
+      children: [
+        {
+          stack: {
+            children: [
+              {
+                component: {
+                  name: 'Overview'
+                }
+              }
+            ],
+            options: {
+              bottomTab: {
+                fontSize: 12,
+                text: 'Cours',
+                iconColor: '#6d3599',
+                selectedIconColor: '#6d3599',
+                icon: CalendarLight,
+                selectedIcon: Calendar
+              },
+              topBar: {
+                visible: true,
+                title: {
+                  component: {
+                    id: 'TopBarOverview',
+                    name: 'TopBarOverview',
+                    alignment: 'fill'
+                  }
+                }
+              }
+            }
+          }
+        },
+        {
+          stack: {
+            children: [
+              {
+                component: {
+                  name: 'Overview'
+                }
+              }
+            ],
+            options: {
+              bottomTab: {
+                fontSize: 12, 
+                text: 'Graphs',
+                iconColor: '#6d3599',
+                selectedIconColor: '#6d3599',
+                icon: ContactLight,
+                selectedIcon: Contact
+              },
+              topBar: {
+                visible: true,
+                title: {
+                  component: {
+                    id: 'TopBarOverview',
+                    name: 'TopBarOverview',
+                    alignment: 'fill'
+                  }
+                }
+              }
+            }
+          }
+        },
+        {
+          stack: {
+            children: [
+              {
+                component: {
+                  name: 'Overview'
+                }
+              }
+            ],
+            options: {
+              bottomTab: {
+                fontSize: 12,
+                text: 'Wallet',
+                iconColor: '#6d3599',
+                selectedIconColor: '#6d3599',
+                icon: ChatLight,
+                selectedIcon: Chat
+              },
+              topBar: {
+                visible: true,
+                title: {
+                  component: {
+                    id: 'TopBarOverview',
+                    name: 'TopBarOverview',
+                    alignment: 'fill'
+                  }
+                }
+              }
+            }
+          }
+        }
+      ]
+    }
+  }
+});*/
--- CryptoTracker/src/scenes/register/register-mail/register-mail.js
+++ CryptoTracker/src/scenes/register/register-mail/register-mail.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Navigation } from 'react-native-navigation';
 import { Text } from 'react-native-elements';
